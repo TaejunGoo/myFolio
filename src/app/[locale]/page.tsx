fb163518f02aecc1cb675420d3847a101759e608
TaejunGoo <48707344+TaejunGoo@users.noreply.github.com>
2025-12-06 17:38:07 +0900

    npm -> pnpm 변경

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,13 @@
-import MSWtest from '@/shared/components/test/MSWtest';
-import { useTranslations } from 'next-intl';
+import { Button } from "@/components/ui/button";
+import MSWtest from "@/shared/components/test/MSWtest";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
-  const t = useTranslations('HomePage');
+  const t = useTranslations("HomePage");
   return (
     <div>
-      <h1>{t('title')}</h1>
+      <h1>{t("title")}</h1>
+      <Button>버튼~~</Button>
       <hr />
       <MSWtest />
     </div>
